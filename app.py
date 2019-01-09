c99d60b0a552aa502b3a43f1c24d7a0b1f809fb1
amit9p <amit_prasad@live.com>
2019-01-09 10:04:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,18 +22,15 @@ def display_msg(msg):
     return msg
 
 
+@app.route('/avro/<s3bucket>/<s3Key>')            
+def avroCount(s3bucket,s3Key):                    
+    s3Key = s3Key.replace(".","/")                
+    s3 = boto3.resource('s3')                     
+    s3obj = s3.Object( s3bucket, s3Key)           
+    filedata= s3obj.get()["Body"].read()          
+    count = filedata.decode('utf8').count('\n')-1 
+    return count                                  
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
-    
-    
-    
-
-
-s3 = boto3.resource('s3')
-s3obj = s3.Object( 'mybucket', 'myfile')
-
-filedata= s3obj.get()["Body"].read()
-
-
-print (filedata.decode('utf8').count('\n')-1)
